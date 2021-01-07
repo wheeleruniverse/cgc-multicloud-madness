f6cc1eec22e7fcfc4120436c6b1ddfbc2822e841
Jwhee <wheeler146@yahoo.com>
2021-01-07 01:55:15 -0500

    Web Files

---
+++ web/capture.js
@@ -0,0 +1,86 @@
+(
+function() {
+
+    // image dimensions
+    var height = 480;
+    var width = 640;    
+         
+    // document elements
+    var button = null;
+    var canvas = null;
+    var video = null;
+    
+    // local state
+    var streaming = false;
+
+
+    function startup() {
+        
+        // find elements
+        button = document.getElementById('start-btn');
+        canvas = document.getElementById('canvas');
+        video = document.getElementById('video');
+
+        // stream user media
+        navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(function(stream) {
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(function(err) {
+            console.log("ERROR: " + err);
+        });
+
+
+        // set image dimensions
+        video.addEventListener('canplay', function(event){
+            
+            if (!streaming) {
+                canvas.setAttribute('width', width);
+                canvas.setAttribute('height', height);
+                video.setAttribute('width', width);
+                video.setAttribute('height', height);
+                streaming = true;
+            }
+            
+        }, false);
+
+
+        // configure click handler to save the picture
+        button.addEventListener('click', function(event){
+            savePicture();
+            event.preventDefault();
+            
+        }, false);
+    }
+
+
+    function savePicture() {
+        
+        canvas.height = height;
+        canvas.width = width;
+        
+        var context = canvas.getContext('2d');
+        context.drawImage(video, 0, 0, width, height);
+
+        $.get('https://v85bx861bh.execute-api.us-east-1.amazonaws.com/uploads', function(data, status){
+            
+            if ("success" === status){
+                
+                $.ajax({
+                    url: data['uploadURL'],
+                    type: 'PUT',
+                    contentType: 'application/octet-stream',  
+                    data: canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream'),
+                    processData: false
+                });
+                
+            }
+            else {
+                console.log("ERROR: " + status);
+            }
+        });
+    }
+
+    window.addEventListener('load', startup, false);
+
+})();
