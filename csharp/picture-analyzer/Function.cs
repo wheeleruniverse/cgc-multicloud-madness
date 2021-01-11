55a74c8cf9138c5f1ed8cf49bd273084bab25d0b
Jwhee <wheeler146@yahoo.com>
2021-01-11 11:28:06 -0500

    Changed NameSpace, S3EventNotification Type, Response Type

--- csharp/picture-analyzer/Function.cs
+++ csharp/picture-analyzer/Function.cs
@@ -1,14 +1,18 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
+using Amazon.S3.Util;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+// [assembly: LambdaSerializer(typeof(JsonSerializer))]
+// [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace picture_analyzer
+namespace Wheeler.PictureAnalyzer
 {
     public class Function
     {
@@ -19,9 +23,47 @@ namespace picture_analyzer
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public List<Response> FunctionHandler(JObject input, ILambdaContext context)
         {
-            return input?.ToUpper();
+
+            List<Response> response = new List<Response>();
+            S3EventNotification s3Event = input.ToObject<S3EventNotification>();
+
+            foreach(var r in s3Event.Records)
+            {
+                response.Add(ProcessRecord(r));
+            }
+            return response;
+        }
+
+
+        private Response ProcessRecord(S3EventNotification.S3EventNotificationRecord record)
+        {
+            // Validate S3 Event
+            string s3Action = record.EventName;
+            string xpAction = "ObjectCreated:Put";
+            if (!s3Action.Equals(xpAction))
+            {
+                string error = $"Expected: {xpAction}; Received: {s3Action}";
+                LambdaLogger.Log(error);
+                return new Response(false, error);
+            }
+
+            // Extract S3 Event Details
+            Response response = new Response
+            {
+                S3Action = s3Action,
+                S3Bucket = record.S3.Bucket.Name,
+                S3Object = record.S3.Object.Key
+            };
+
+            // TODO: Load S3 Object
+
+            // TODO: Send File to GCP Vision API
+
+            // TODO: Write GCP Vision API Results to Azure Tables
+
+            return response;
         }
     }
 }
---
+++ csharp/picture-analyzer/Response.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Wheeler.PictureAnalyzer
+{
+    public class Response
+    {
+        public string S3Action { get; set; }
+        public string S3Bucket { get; set; }
+        public string S3Object { get; set; }
+        public bool Success { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public Response(bool Success, string ErrorMessage)
+        {
+            this.Success = Success;
+            this.ErrorMessage = ErrorMessage;
+        }
+        public Response() : this(true, null)
+        { 
+            // Default to Success
+        }
+    }
+}
