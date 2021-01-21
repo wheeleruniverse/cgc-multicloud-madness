ec2d7e746fd721fbdf6c2f434f545752e83f6a53
Jwhee <wheeler146@yahoo.com>
2021-01-21 09:36:57 -0500

    Changed azure-code to Use Axios instead of Https Request, Updated .gitignore, Updated SAM Template

--- sam/azure-code/index.js
+++ sam/azure-code/index.js
@@ -1,7 +1,8 @@
 'use strict'
 
+const axios = require('axios');
 const auth = require('creds/auth.json');
-const https = require('https')
+
 
 // AWS Lambda entry point
 exports.handler = async (event) => {
@@ -10,35 +11,31 @@ exports.handler = async (event) => {
 
 const queryAzureAnalysisTable = async function(event) {
     
-    const limit = `$filter=PartitionKey%20eq%20'${randomNumberBetween(1, 10)}'&$top=50`
-    console.log(`limit: ${limit}`);
-    
-    const options = {
+    const filter = `PartitionKey%20eq%20'${randomNumberBetween(1, 10)}'`
+    console.log(`filter: ${filter}`);
+
+    axios.get(auth.url, {
         headers: {
             'Accept': 'application/json;odata=nometadata'
         },
-        host: auth.Host,
-        path: `${auth.Path}&${limit}` 
-    }
-    
-    let response;
-    const req = https.request(options, res => {
-        console.log(`statusCode: ${res.statusCode}`)
-
-        res.on('data', d => {
-            response = d.value;
-        })
+        params: {
+            '$filter': filter,
+            '$top': '50',
+            'si': auth.si,
+            'sig': auth.sig,
+            'sv': auth.sv,
+            'tn': auth.tn,
+        }
     })
-
-    req.on('error', error => {
-        console.error(error)
-        response = [];
+    .then(function (response) {
+        console.log(response);
+        console.log(`Found ${response.length} Records`);
+        return response;
     })
-
-    req.end()
-    
-    console.log(`Found ${response.length} Records`);
-    return response;
+    .catch(function (error) {
+        console.error(error);
+        return error;
+    })
 }
 
 
